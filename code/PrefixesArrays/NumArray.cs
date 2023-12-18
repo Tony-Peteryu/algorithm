3d60715306ad3672ae4d6a1c52c7132512b283e2
Tony-Peteryu <1825198200@qq.com>
2023-12-18 14:08:13 +0800

    add prefixesArrays code

---
+++ code/PrefixesArrays/NumArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrefixesArrays
+{
+    public class NumArray
+    {
+        public int[] nums { get; set; }
+        public NumArray(int[] nums)
+        {
+            this.nums = new int[nums.Length + 1];
+            this.nums[0] = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                this.nums[i + 1] = this.nums[i] + nums[i];
+            }
+        }
+
+        public int SumRange(int left, int right)
+        {
+            return nums[right + 1] - nums[left];
+        }
+    }
+}
---
+++ code/PrefixesArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrefixesArrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NumArray numArray = new NumArray(new int[] { -2, 0, 3, -5, 2, -1 });
+            Console.WriteLine(numArray.SumRange(0, 2));
+            Console.WriteLine(numArray.SumRange(2, 5));
+            Console.WriteLine(numArray.SumRange(0, 5));
+        }
+    }
+}
