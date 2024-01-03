418eb03c82a52bfcb13ccbad137ec750e61606f3
Tony-Peteryu <1825198200@qq.com>
2024-01-03 11:16:24 +0800

    commit code

--- code/PrefixesArrays/Program.cs
+++ code/PrefixesArrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PrefixesArrays
 {
@@ -6,10 +7,38 @@ namespace PrefixesArrays
     {
         public static void Main(string[] args)
         {
-            NumArray numArray = new NumArray(new int[] { -2, 0, 3, -5, 2, -1 });
-            Console.WriteLine(numArray.SumRange(0, 2));
-            Console.WriteLine(numArray.SumRange(2, 5));
-            Console.WriteLine(numArray.SumRange(0, 5));
+            
+        }
+
+        public bool IsAcronym(IList<string> words, string s)
+        {
+            if (words.Count != s.Length) { return false; }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != words[i][0]) 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static IList<int> NumOfBurgers(int tomatoSlices, int cheeseSlices) 
+        {
+            List<int> result = new List<int>();
+            if (tomatoSlices % 2 != 0)
+            {
+                return result;
+            }
+            if (tomatoSlices >= cheeseSlices * 2 && tomatoSlices <= cheeseSlices * 4)
+            {
+                int total_jumbo = (tomatoSlices - 2 * cheeseSlices) / 2;
+                int total_small = cheeseSlices - total_jumbo;
+                result.Add(total_jumbo);
+                result.Add(total_small);
+                return result;
+            }
+            return result;
         }
     }
 }
